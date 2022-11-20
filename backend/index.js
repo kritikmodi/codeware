4d5a67388354674cdd271883ac8dc1f2f0dabf97
kritikmodi <kritikmodi@gmail.com>
2022-11-21 00:27:21 +0530

    Add port

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
-const express = require('express');
+const express = require("express");
 
-const app = express;
+const app = express();
 
 app.listen(5000, () => {
    console.log('Listening on port 5000!');
