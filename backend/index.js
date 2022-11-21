a1fac19ef302a254e095b7540bf909ea88928eb7
kritikmodi <kritikmodi@gmail.com>
2022-11-22 00:43:21 +0530

    Add GET route

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.get('/', (req,res) => {
+   return res.json({Hello: "world!"});
+});
+
 app.listen(5000, () => {
    console.log('Listening on port 5000!');
 });
