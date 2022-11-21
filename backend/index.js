f197fc5e635d2467d31a98406a8c6a1e5585b20d
kritikmodi <kritikmodi@gmail.com>
2022-11-22 00:45:15 +0530

    Add POST route

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,10 @@ app.get('/', (req,res) => {
    return res.json({Hello: "world!"});
 });
 
+app.post('/run', (req,res) => {
+   return res.json(req.body);
+});
+
 app.listen(5000, () => {
    console.log('Listening on port 5000!');
 });
