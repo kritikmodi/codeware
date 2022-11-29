210b6cf8e1fbcfa1f106dd672e595c737b580f3b
kritikmodi <kritikmodi@gmail.com>
2022-11-29 23:30:42 +0530

    Add file directory check

--- backend/generateFile.js
+++ backend/generateFile.js
@@ -4,6 +4,9 @@ const path = require('path');
 
 const dirCodes = path.join(__dirname, "codes");
 
+if(!fs.existsSync(dirCodes)){
+  fs.mkdirSync(dirCodes, {recursive: true});
+}
 const generateFile = async (format, code) => {
   
 };
