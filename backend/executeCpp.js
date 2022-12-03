c16c13814a24f01c0867642104bb0b01dece78f8
kritikmodi <kritikmodi@gmail.com>
2022-12-03 23:50:25 +0530

    Add output path

--- backend/executeCpp.js
+++ backend/executeCpp.js
@@ -1,4 +1,11 @@
 const {exec} = require('child_process');
+const fs = require('fs');
+
+const outputPath = path.join(__dirname, "outputs");
+
+if(fs.existsSync(outputPath)){
+    fs.mkdirSync(outputPath, {recursive : true});
+}
 
 const executeCpp = (filepath) => {
 
