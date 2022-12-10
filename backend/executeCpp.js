62df5ef5b2e4634e9a9129034acd24027ef7f7ca
kritikmodi <kritikmodi@gmail.com>
2022-12-11 00:27:38 +0530

    Revert some changes

--- backend/executeCpp.js
+++ backend/executeCpp.js
@@ -9,7 +9,7 @@ if(fs.existsSync(outputPath)){
 }
 
 const executeCpp = (filepath) => {
-    const jobId = path.basename(filepath).split(".")[1];
+    const jobId = path.basename(filepath).split(".")[0];
     const outPath = path.join(outputPath, `${jobId}.out`);
     return new Promise((resolve, reject) => {
         exec(`g++ ${filepath} -o ${outPath} && cd ${outputPath} && ./${jobId}.out`,
