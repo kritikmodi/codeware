6605b355c3b81d468589c0b016e1c3164d1004d6
kritikmodi <kritikmodi@gmail.com>
2022-12-12 00:00:50 +0530

    Increase textarea dims

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>Online Code Compiler</h1>
-      <textarea></textarea>
+      <textarea>rows='20' cols='75'</textarea>
       <br>
       <button>Submit</button>
       </br>
