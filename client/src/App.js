b6be4231e12630586f0c6d0c8413fbd5011f36f2
kritikmodi <kritikmodi@gmail.com>
2022-12-14 22:50:12 +0530

    Fix design

--- client/src/App.js
+++ client/src/App.js
@@ -5,10 +5,9 @@ function App() {
   return (
     <div className="App">
       <h1>Online Code Compiler</h1>
-      <textarea>rows='20' cols='75'</textarea>
-      <br>
+      <textarea rows="20" cols="75"></textarea>
+      <br/>
       <button>Submit</button>
-      </br>
     </div>
   );
 }
