17abf928615b5bd168d1fa9d32ce36f6066e7dbd
kritikmodi <kritikmodi@gmail.com>
2022-12-14 22:55:21 +0530

    Add onChange listener for the textarea

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <h1>Online Code Compiler</h1>
-      <textarea rows="20" cols="75"></textarea>
+      <textarea rows="20" cols="75" value={code} onChange={(e)=>{setCode(e.target.value)}}></textarea>
       <br/>
       <button>Submit</button>
     </div>
