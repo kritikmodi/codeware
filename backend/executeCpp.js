a164176bd6e6deb34b169d75a88007c17730ca73
Kritik Modi <61862301+kritikmodi@users.noreply.github.com>
2022-12-20 23:09:42 +0530

    Remove outputPath

--- backend/executeCpp.js
+++ backend/executeCpp.js
@@ -2,8 +2,6 @@ const {exec} = require('child_process');
 const fs = require('fs');
 const path = require('path');
 
-const outputPath = path.join(__dirname, "outputs");
-
 if(fs.existsSync(outputPath)){
     fs.mkdirSync(outputPath, {recursive : true});
 }
