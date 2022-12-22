2464ad666059f6cbf131c7de7d8200bfd41b4078
kritikmodi <kritikmodi@gmail.com>
2022-12-23 01:20:22 +0530

    Improve the user interface

--- client/src/App.js
+++ client/src/App.js
@@ -24,8 +24,12 @@ function App() {
 
   return (
     <div className="App">
+      <br/>
       <h1>Online Code Compiler</h1>
+      <br/>
       <textarea rows="20" cols="75" value={code} onChange={(e)=>{setCode(e.target.value)}}></textarea>
+      <br/>
+      <br/>
       <br/>
       <button onClick={handleSubmit}>Submit</button>
       <p>{output}</p>
