dc4532d6220427013615c1ba73dbea0cc3aa68ad
kritikmodi <kritikmodi@gmail.com>
2022-12-23 01:31:44 +0530

    Add axios library

--- client/src/App.js
+++ client/src/App.js
@@ -27,7 +27,7 @@ function App() {
       <br/>
       <h1>Online Code Compiler</h1>
       <br/>
-      <textarea rows="20" cols="75" value={code} onChange={(e)=>{setCode(e.target.value)}}></textarea>
+      <textarea rows="20" cols="75" value={code} onChange={(e)=>{setCode(e.target.value);}}></textarea>
       <br/><br/><br/>
       <button onClick={handleSubmit}>Submit</button>
       <p>{output}</p>
