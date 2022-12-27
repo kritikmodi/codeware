db7d4ae16b79ae37e107f966fcabec1e66200170
Kritik Modi <61862301+kritikmodi@users.noreply.github.com>
2022-12-27 22:28:59 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -34,6 +34,7 @@ Overall, a three-way communication takes place when the user tries to run a code
 
 */
 
+// This is the post/run route which is responsible for getting the code to the backend.
 app.post("/run", async (req,res) => {
    const {language="cpp",code} = req.body;
 
