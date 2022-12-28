d86ad062c1718c3b3658892de23f436a1f6ab793
Kritik Modi <61862301+kritikmodi@users.noreply.github.com>
2022-12-28 23:47:04 +0530

    Add comments

--- backend/generateFile.js
+++ backend/generateFile.js
@@ -1,15 +1,22 @@
+// The fs module stands for 'file system' and is used to perform multiple file operations such as reading, writing, creating and deleting files.
 const fs = require('fs');
 
+// The path module is used for working with directories and file paths.
 const path = require('path');
 
+// The uuid module is used for generating unique ids.
+// 'v4:uuid' means that 'v4' is renamed/stored as 'uuid'.
 const {v4:uuid} = require('uuid');
 
+// The __dirname variable stores the directory name of the current module.
 const dirCodes = path.join(__dirname, "codes");
 
+// If the 'dirCodes' folder doesn't exist, it is created using the 'mkdir' function.
 if(!fs.existsSync(dirCodes)){
   fs.mkdirSync(dirCodes, {recursive: true});
 }
 
+// The following function generates a cpp file inside the 'dircodes' folder and writes the received code in it.
 const generateFile = async (format, content) => {
   const jobId = uuid();
   const filename = `${jobId}.${format}`;
@@ -18,6 +25,7 @@ const generateFile = async (format, content) => {
   return filepath;
 };
 
+// The module.exports section contains the functions which are allowed to be 'exported' or used by external scripts or functions.
 module.exports = {
   generateFile,
 };
