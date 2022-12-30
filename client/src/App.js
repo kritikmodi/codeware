fcd0298a78f18174fea8fe3b9ba21d9074099cbe
Kritik Modi <61862301+kritikmodi@users.noreply.github.com>
2022-12-30 23:06:58 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,7 @@
+// This is the main file of the frontend.
+
 import './App.css';
+// 'useState' hooks are used for storing and updating a variables value based on certain conditions.
 import React, {useState} from 'react';
 import axios from 'axios';
 
