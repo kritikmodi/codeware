a1d6fd548ecf86a93e6f7db439e40bd70a998d00
Kritik Modi <61862301+kritikmodi@users.noreply.github.com>
2023-01-11 22:28:46 +0530

    Update 6301f2c7-f123-4b4f-82cd-e9ee951909bb.cpp

--- backend/codes/6301f2c7-f123-4b4f-82cd-e9ee951909bb.cpp
+++ backend/codes/6301f2c7-f123-4b4f-82cd-e9ee951909bb.cpp
@@ -4,13 +4,6 @@
 using namespace std;
 
 int main(){
-	int a=0,b=1,count=10;
-	for (int i = 0; i < count; ++i)
-	{
-            cout << a << endl;
-            c=b;
-            b = a + b;
-	    a = c;
-	}
+return 0;
 }
 
