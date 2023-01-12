a84b75a28b5ab82a777f504a9b740fd59025b7a9
kritikmodi <kritikmodi@gmail.com>
2023-01-13 01:08:02 +0530

    Add nodemon

--- backend/index.js
+++ backend/index.js
@@ -38,7 +38,7 @@ Overall, a three-way communication takes place when the user tries to run a code
 app.post("/run", async (req,res) => {
    const {language="cpp",code} = req.body;
    console.log(language);
-   console.log(code);
+   console.log(code.length);
 
 // The following condition checks if the code body is empty or undefined etc..
    if(!code){
