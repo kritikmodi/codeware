f751da2ac6e618d10acda74f4997dc1a365bbb0b
kritikmodi <kritikmodi@gmail.com>
2023-01-16 00:53:29 +0530

    Edit comments

--- backend/executePy.js
+++ backend/executePy.js
@@ -1,7 +1,7 @@
 // Th exec module is used to execute shell commands through our own scripts.
 const {exec} = require('child_process');
 
-// The following function is responsible for executing the cpp code using the exec command and storing the output in the 'outputs' folder and also returning it back to the client.
+// The following function is responsible for executing the python code using the exec command and storing the output in the 'outputs' folder and also returning it back to the client.
 const executePy = (filepath) => {
     return new Promise((resolve, reject) => {
         exec(`python ${filepath}`,
