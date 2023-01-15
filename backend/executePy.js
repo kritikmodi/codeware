a043ccc092a82227fcbf384a16d108ba3f3d2469
kritikmodi <kritikmodi@gmail.com>
2023-01-16 00:29:10 +0530

    Remove jobID and outputPath from executePy.js

--- backend/executePy.js
+++ backend/executePy.js
@@ -5,8 +5,6 @@ const path = require('path');
 
 // The following function is responsible for executing the cpp code using the exec command and storing the output in the 'outputs' folder and also returning it back to the client.
 const executeCpp = (filepath) => {
-    const jobId = path.basename(filepath).split(".")[0];
-    const outPath = path.join(outputPath, `${jobId}.out`);
     return new Promise((resolve, reject) => {
         exec(`g++ ${filepath} -o ${outPath} && cd ${outputPath} && ./${jobId}.out`,
                 (error, stdout, stderr) => {
