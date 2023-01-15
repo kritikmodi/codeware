fb6d830d86817ad58f7b5739c2823aad2c1d1d7a
kritikmodi <kritikmodi@gmail.com>
2023-01-16 00:43:31 +0530

    Remove unrequired modules from executePy.js

--- backend/executePy.js
+++ backend/executePy.js
@@ -1,7 +1,5 @@
 // Th exec module is used to execute shell commands through our own scripts.
 const {exec} = require('child_process');
-const fs = require('fs');
-const path = require('path');
 
 // The following function is responsible for executing the cpp code using the exec command and storing the output in the 'outputs' folder and also returning it back to the client.
 const executePy = (filepath) => {
