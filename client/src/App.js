1c32a9e871124659db446fe20250d418224c20a0
kritikmodi <kritikmodi@gmail.com>
2023-01-29 01:11:25 +0530

    Update function variables

--- client/src/App.js
+++ client/src/App.js
@@ -10,10 +10,10 @@ function App() {
 
   // This useState hook is used for saving the code state.
   const [code,setCode] = useState("");
-  // This useState hook is used for updating the output once computed.
-  const [output,setOutput] = useState("");
   // This useState hook is used for updating the programming language for the compiler.
   const [language,setLanguage] = useState("cpp");
+  // This useState hook is used for updating the output once computed.
+  const [output,setOutput] = useState("");
 
   const handleSubmit = async () => {
 
