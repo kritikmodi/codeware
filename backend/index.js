3e503a7d2a9d6fd82fe8101dab587176e5bb5591
Kritik Modi <61862301+kritikmodi@users.noreply.github.com>
2023-02-07 23:26:02 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ main().catch(err => console.log(err));
 
 async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/online-code-compiler');
-  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+  // use `await mongoose.connect('mongodb://kritikmodi:xxxxx@127.0.0.1:27017/test');`
 }
 
 app.use(cors());
