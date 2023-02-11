8f1589fe24a827a2179373c41acd7ee68cfe8065
kritikmodi <kritikmodi@gmail.com>
2023-02-11 22:22:07 +0530

    Add language in JobSchema

--- backend/models/Job.js
+++ backend/models/Job.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
 
 const JobSchema = mongoose.Schema({
-
+    language: {
+        type: String,
+        required: true,
+        enum: ["cpp", "py"]
+    }
 });
 
 const Job = new mongoose.model('job', JobSchema);
