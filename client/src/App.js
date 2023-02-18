a9a88870e3e8249de63204b3c14b0be2a2ab288d
Kritik Modi <61862301+kritikmodi@users.noreply.github.com>
2023-02-18 22:50:20 +0530

    Change output variable

--- client/src/App.js
+++ client/src/App.js
@@ -24,7 +24,8 @@ function App() {
     
     try{
       const {data} = await axios.post("http://localhost:5000/run", payload);
-      setOutput(data.output);
+      console.log(data);
+      setOutput(data.jobId);
     }catch({response}){
       if(response){
         const errMsg=response.data.err.stderr;
