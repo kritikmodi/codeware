291aea35a87c447c8a12458ef890b24926ce4492
Kritik Modi <61862301+kritikmodi@users.noreply.github.com>
2023-02-20 23:26:05 +0530

    Update get request

--- backend/index.js
+++ backend/index.js
@@ -45,7 +45,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.json());
 
-app.get("/", (req,res) => {
+app.get("/status", async(req,res) => {
    return res.json({GET: "Request!"});
 });
 
