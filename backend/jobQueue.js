8d338f5f417a50c9100481a97614cbe1912fd474
kritikmodi <kritikmodi@gmail.com>
2023-03-10 02:04:31 +0530

    Add export function

--- backend/jobQueue.js
+++ backend/jobQueue.js
@@ -2,5 +2,9 @@ const Queue = require("bull");
 const jobQueue = new Queue('job-queue');
 
 const addJobToQueue = async(jobId) => {
-    await jobQueue.add({jobId});
-}
+    await jobQueue.add({id:jobId});
+};
+
+module.exports = {
+    addJobToQueue
+};
