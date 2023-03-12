bba65dcf2b46b18dd4950d00f7f801443769f227
Kritik Modi <61862301+kritikmodi@users.noreply.github.com>
2023-03-13 00:24:15 +0530

    Add jobQueue.process

--- backend/jobQueue.js
+++ backend/jobQueue.js
@@ -1,6 +1,12 @@
 const Queue = require("bull");
 const jobQueue = new Queue('job-queue');
 
+const NUM_WORKERS = 5;
+
+jobQueue.process(NUM_WORKERS, () => {
+    
+});
+
 const addJobToQueue = async(jobId) => {
     await jobQueue.add({id:jobId});
 };
