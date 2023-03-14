f0a87854107b511c95cebd90664e1bc45267d441
Kritik Modi <61862301+kritikmodi@users.noreply.github.com>
2023-03-14 23:55:58 +0530

    Add id and assign job variable

--- backend/jobQueue.js
+++ backend/jobQueue.js
@@ -7,6 +7,8 @@ const Job = require('./models/Job');
 
 jobQueue.process(NUM_WORKERS, async ({data}) => {
     console.log(data);
+    const {id : jobId} = data;
+    const job = await Job.findById(jobId);
 });
 
 const addJobToQueue = async(jobId) => {
